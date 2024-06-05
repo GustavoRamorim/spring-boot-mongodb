2f8d8a2bce5c0022f9f88ee0eb2b335ba6ce2a92
GustavoRamorim <rabelocarservice@hotmail.com>
2024-06-05 11:53:57 -0300

    Atualização de usuário com PUT

--- src/main/java/com/gustavorabelo/mongo/MongoApplication.java
+++ src/main/java/com/gustavorabelo/mongo/MongoApplication.java
@@ -3,6 +3,7 @@ package com.gustavorabelo.mongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MongoApplication {
 
