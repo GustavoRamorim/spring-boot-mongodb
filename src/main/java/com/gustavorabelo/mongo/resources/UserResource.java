3da615e1db64e0cc35540a73bc746ee9e6fd75df
GustavoRamorim <rabelocarservice@hotmail.com>
2024-06-05 12:38:43 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/gustavorabelo/mongo/resources/UserResource.java
+++ src/main/java/com/gustavorabelo/mongo/resources/UserResource.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gustavorabelo.mongo.domain.Post;
 import com.gustavorabelo.mongo.domain.User;
 import com.gustavorabelo.mongo.dto.UserDTO;
 import com.gustavorabelo.mongo.service.UserService;
@@ -60,4 +60,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
