0b3912e1915782c6113601501e65e9f27c35b0b8
GustavoRamorim <rabelocarservice@hotmail.com>
2024-06-05 12:54:21 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/gustavorabelo/mongo/config/Instantiation.java
+++ src/main/java/com/gustavorabelo/mongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.gustavorabelo.mongo.domain.Post;
 import com.gustavorabelo.mongo.domain.User;
 import com.gustavorabelo.mongo.dto.AuthorDTO;
+import com.gustavorabelo.mongo.dto.CommentDTO;
 import com.gustavorabelo.mongo.repository.PostRepository;
 import com.gustavorabelo.mongo.repository.UserRepository;
 
@@ -42,7 +43,15 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
